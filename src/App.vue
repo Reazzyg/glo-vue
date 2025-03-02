75693fcc08efbf10fab80eb884ed2dc8294ee898
Semion Mudrov <mudrsemen@ya.ru>
2025-03-02 23:10:08 +0300

    Feat: added some components to page

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <template>
-  <div id="app">
-   hello vue.js
-  </div>
+<div class="wrapper">
+  <hero-view/>
+  <footer-component/>
+</div>
 </template>
 
 <script>
-
+import HeroView from './views/HeroView.vue';
+import FooterComponent from './components/FooterComponent.vue';
 export default {
- 
+ components: {HeroView, FooterComponent}
 }
 </script>
 
